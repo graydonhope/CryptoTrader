2f21c190cc3502c242dc69e4b08383997dcb438b
Graydon Hope <graydonho@gmail.com>
2019-07-14 03:57:28 -0400

    Fixed top navigation bar and ad block detection

--- scripts/detail.js
+++ scripts/detail.js
@@ -6,5 +6,3 @@ function showFallbackImage() {
     var popupMessage = "This website uses a widget which gets blocked by common adblockers. \n \n Please turn off adblock for this website. \n \n Trust us! There are no hidden ads!! :)";
     alert(popupMessage);
 }
-
-
