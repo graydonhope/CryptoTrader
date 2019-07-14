448a2e06371b6a4d98b8c94d13f5196181357f3f
Graydon Hope <graydonho@gmail.com>
2019-07-14 19:23:59 -0400

    JavaScript functionality to Register page

---
+++ scripts/create_account.js
@@ -0,0 +1,103 @@
+var validName = false;
+var validEmail = false;
+var validPassword = false;
+
+function createAccount() {
+    // Check for valid user inputs
+    checkName();
+    validateEmail();
+    checkPasswords();
+
+    if (validName && validEmail && validPassword) {
+        // All is good, successful create account
+        alert("Account Successfully Created!");
+        window.location.href = "market.html";
+        return false;
+    }
+    else {
+        // Stay on page
+        return false;
+    }
+}
+
+function checkName() {
+    // Ensure the user has entered a name with only letters
+    var nameEntered = document.getElementById("fullname").value;
+
+    // Remove whitespace from name entered
+    nameEntered = nameEntered.replace(/ /g, '');
+    
+    // Check if only letters 
+    if ((nameEntered == "") || (/^[a-z]+$/i.test(nameEntered))) {
+        document.getElementById("fullname-error").innerHTML = "";  
+        document.getElementById("fullname").style.border = "none";
+        validName = false;
+    }
+    else {
+        var errorMessage = "Please enter letters only."
+        document.getElementById("fullname-error").innerHTML = errorMessage;  
+        document.getElementById("fullname").style.border = "2px solid red";
+        validName = false;
+    }
+
+    if ((nameEntered != "") && (/^[a-z]+$/i.test(nameEntered))) {
+        validName = true;
+    }
+}
+
+function validateEmail() {
+    var email = document.getElementById("email").value;
+    var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    if (!re.test(String(email).toLowerCase())) {
+        document.getElementById("email").style.border = "2px solid red";
+        validEmail = false;
+    }
+    else {
+        document.getElementById("email").style.border = "none";
+        validEmail = true;
+    }
+}
+
+function checkPasswords() {
+    // Check if both password fields have been filled out and that they match.
+    var password = document.getElementById("password").value;
+    var confirmedPassword = document.getElementById("create-account__confirm-password").value;
+
+    if ( (password != "") && (confirmedPassword != "") && (password === confirmedPassword) ) {
+        validPassword = true;
+        document.getElementById("password-error").innerHTML = "";
+    }
+    else{
+        var errorMessage = "Passwords must match"
+        document.getElementById("password-error").innerHTML = errorMessage;
+        document.getElementById("password-error").style.color = "red";
+    }
+}
+
+function emailFocus() {
+    if (validEmail) {
+        document.getElementById("email").style.boxShadow = "none";
+        document.getElementById("email").style.border = "2px solid black";
+    }
+    else if (!document.getElementById("email").value == ""){
+        document.getElementById("email").style.boxShadow = "none";
+        document.getElementById("email").style.border = "2px solid red";
+    }
+    else {
+        document.getElementById("email").style.border = "none";
+    }
+}
+
+function nameFocus() {
+    if (validName) {
+        document.getElementById("fullname").style.boxShadow = "none";
+        document.getElementById("fullname").style.border = "2px solid black";
+    }
+    else if (!document.getElementById("fullname").value == ""){
+        document.getElementById("fullname").style.boxShadow = "none";
+        document.getElementById("fullname").style.border = "2px solid red";
+    }
+    else {
+        document.getElementById("fullname").style.border = "none";
+    }
+}
